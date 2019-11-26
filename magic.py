9fa14ced6513a103e4cb81114fa87c2c5a3976ee
Vlod134 <55443799+Vlod134@users.noreply.github.com>
2019-11-26 14:21:05 +0300

    Add files via upload

---
+++ magic.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from random import*
+import time
+import math
+
+class effect():
+	def __init__(self, char, amount):
+		self.char=char
+		self.amount=amount
+
+	def applyEffect(self,unit)
+		if (self.char == 'atc'):
+			unit.atc += amount
+
+class magic():
+	def __init__(self, school, image, cost):
+		self.school = school
+		self.image = image
+		self.cost = cost
+	
+	def Cost(self, hero)
+		hero.mana = hero.mana - cost
+
+	def cast(self,unit):
+
+
+
+class damagemagic(magic):
+	def __init__ (self, damage, school, image, cost):
+		magic.__init__ (self, school, image, cost)
+		self.damage = damage
+
+	def cast(self, unit):
+		unit.hp = unit.hp - self.damage
+		
+		
+class effectmagic(magic)
+	def __init__ (self, effects, school, image, cost):
+		magic.__init__ (self, school, image, cost)
+		self.effects = effects
+
+
+	def cast(self, unit):
+		for eff in self.effects:
+			unit.effect.append(eff)
+
