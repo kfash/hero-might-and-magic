ff861193761ceaf716f206068281821abfdf295f
Vlod134 <55443799+Vlod134@users.noreply.github.com>
2019-12-03 15:09:23 +0300

    убрал весь мусор

--- magic.py
+++ magic.py
@@ -1,46 +1,29 @@
 import tkinter as tk
-from random import*
+from random import *
 import time
 import math
 
+#эффект(баф или дебаф юнита)
 class effect():
 	def __init__(self, char, amount):
-		self.char=char
-		self.amount=amount
+		self.char = char
+		self.amount = amount
 
-
-	def applyEffect(self,unit):
-		exec("unit."+self.char+"+="+self.amount)
+	def applyEffect(self, unit):
+		exec("unit." + self.char + "+=" + self.amount)
 
 
 class magic():
-	def __init__(self, school, image, cost):
+	def __init__(self, effects, school, image, cost):
+		self.effects = effects
 		self.school = school
 		self.image = image
 		self.cost = cost
-	
-	def Cost(self, hero):
-		hero.mana = hero.mana - cost
 
-	def cast(self,unit):
-
-
-
-class damagemagic(magic):
-	def __init__ (self, damage, school, image, cost):
-		magic.__init__ (self, school, image, cost)
-		self.damage = damage
-
-	def cast(self, unit):
-		unit.hp = unit.hp - self.damage
-		
-		
-class effectmagic(magic):
-	def __init__ (self, effects, school, image, cost):
-		magic.__init__ (self, school, image, cost)
-		self.effects = effects
-
+#применение заклинания
 	def cast(self, unit):
 		for eff in self.effects:
 			unit.effect.append(eff)
 
+	def cost(self, hero):
+		hero.mana = hero.mana - cost
