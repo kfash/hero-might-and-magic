b5af1be3131f6c2adc4bdce78202ead3a28701ae
strangehood <55440847+strangehood@users.noreply.github.com>
2019-12-09 17:14:14 +0300

    Ошибка в spell.py низвестная функция rand

--- spell.py
+++ spell.py
@@ -3,25 +3,26 @@ from random import randrange as rnd
 
 
 def magic_arrow(unit, hero):
-	unit.hpat -= 20 + hero.magicforce*10
+	unit.hpat -= 20 + hero.magicforce * 10
 	unit.recount_num
 	
 	hero.mana -= 5
 
 def thirst_for_blood(unit, hero):
-	ef=effect('atk',6,hero.mageforce/2)
+	ef=effect('atk', 6, hero.mageforce / 2)
 	unit.effect.append(ef)
 
 	hero.mana -= 5
-	
+
+# FIXME: ошибка в rand
 def curse(unit, hero):
-	ef = effect('rand', -rand, hero.mageforce/2)
+	ef = effect('rand', - rand, hero.mageforce/2)
 	unit.effect.append(ef)
 	
 	hero.mana -= 6
 
 def rabies(unit, hero):
-	ef1 = effect('atk', 1,5*unit.defe, 1)
+	ef1 = effect('atk', 1, 5 * unit.defe, 1)
 	ef2 = effect('defe', -unit.defe, 1)
 	unit.effect.append(ef1)
 	unit.effect.append(ef2)
@@ -29,21 +30,21 @@ def rabies(unit, hero):
 	hero.mana -= 16
 	
 def failure(unit, hero):
-	ef = effect('luck', -2, hero.mageforce/2)
+	ef = effect('luck', -2, hero.mageforce / 2)
 	unit.effect.append(ef)
 
 	hero.mana -= 12
 	
 def blessing(unit, hero):
-	ef1 = effect('rand', -unit.rand, hero.mageforce/2)
-	ef2 = effect('damage', unit.rand, hero.mageforce/2)
+	ef1 = effect('rand', -unit.rand, hero.mageforce / 2)
+	ef2 = effect('damage', unit.rand, hero.mageforce / 2)
 	unit.effect.append(ef1)
 	unit.effect.append(ef2)
 	
 	hero.mana -= 5
 	
 def treatment(unit, hero):
-	unit.hpat += 20 + hero.magicforce*5	
+	unit.hpat += 20 + hero.magicforce * 5
 	
 	while unit.effect:
 		unit.effect.pop(-1)
@@ -57,19 +58,19 @@ def snaping(unit, hero):
 	hero.mana -= 5
 	
 def ice_arrow(unit, hero):
-	unit.hpat -= 20 + hero.magicforce*20
+	unit.hpat -= 20 + hero.magicforce * 20
 	unit.recount_num
 	
 	hero.mana -= 8
 	
 def weakness(unit, hero):
-	ef = effect('atk', -6, hero.mageforce/2)
+	ef = effect('atk', -6, hero.mageforce / 2)
 	unit.effect.append(ef)
 	
 	hero.mana -= 8
 	
 def joy(unit, hero):
-	ef = effect('moral', 2, hero.mageforce/2)
+	ef = effect('moral', 2, hero.mageforce / 2)
 	unit.effect.append(ef)
 
 	hero.mana -= 12
@@ -77,46 +78,45 @@ def joy(unit, hero):
 def teleport(unit, hero):
 	unit.x = rnd(1, 12)
 	unit.y = rnd(1, 12)
-	
+
 	hero.mana -= 10
 	
 def prayer(unit, hero):
-	ef1 = effect('atk', 4, hero.mageforce/2)
-	ef2 = effect('defe', 4, hero.mageforce/2)
-	ef3 = effect('speed', 4, hero.mageforce/2)
+	ef1 = effect('atk', 4, hero.mageforce / 2)
+	ef2 = effect('defe', 4, hero.mageforce / 2)
+	ef3 = effect('speed', 4, hero.mageforce / 2)
 	unit.effect.append(ef1)
 	unit.effect.append(ef2)
 	unit.effect.append(ef3)
 
-	
 	hero.mana -= 16
 	
 def slowness(unit, hero):
-	ef = effect('speed', -unit.speed/2, hero.mageforce/2)
+	ef = effect('speed', -unit.speed/2, hero.mageforce / 2)
 	unit.effect.append(ef)
 	
 	hero.mana -= 6
 	
 def stone_skin(unit, hero):
-	ef = effect('defe', 6, hero.mageforce/2)
+	ef = effect('defe', 6, hero.mageforce / 2)
 	unit.effect.append(ef)
 	
 	hero.mana -= 5
 	
 def sadness(unit, hero):
-	ef = effect('moral', -2, hero.mageforce/2)
+	ef = effect('moral', -2, hero.mageforce / 2)
 	unit.effect.append(ef)
 	
 	hero.mana -= 4
 	
 def explosion(unit, hero):
-	unit.hpat -= 200 + hero.magicforce*75
+	unit.hpat -= 200 + hero.magicforce * 75
 	unit.recount_num
 	
 	hero.mana -= 30
 	
 def acceleration(unit, hero):
-	ef = effect('speed', 5, hero.mageforce/2)
+	ef = effect('speed', 5, hero.mageforce / 2)
 	unit.effect.append(ef)
 	
 	hero.mana -= 6
@@ -128,13 +128,13 @@ def destructive_beam(unit, hero):
 	hero.mana -= 10
 	
 def luck(unit, hero):
-	ef = effect('luck', 2, hero.mageforce/2)
+	ef = effect('luck', 2, hero.mageforce / 2)
 	unit.effect.append(ef)
 	
 	hero.mana += 2
 	
 def lightning (unit, hero):
-	unit.hpat -= 20 + hero.magicforce*25
+	unit.hpat -= 20 + hero.magicforce * 25
 	unit.recount_num
 	
 	hero.mana -= 10
